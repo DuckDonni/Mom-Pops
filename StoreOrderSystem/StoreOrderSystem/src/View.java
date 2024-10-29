e1cfbf72ac63cf5776af79abf4bbb013b09dc771
DuckDonni <jonahsmith2010@gmail.com>
2024-10-29 16:18:27 -0400

    Fixed alignment with nav bar and home page

--- StoreOrderSystem/StoreOrderSystem/src/View.java
+++ StoreOrderSystem/StoreOrderSystem/src/View.java
@@ -1,20 +1,22 @@
 import javax.swing.*;
 import net.miginfocom.swing.MigLayout;
-
+import java.awt.*;
 public class View {
 
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Homepage Example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-        frame.setLayout(new MigLayout());
+        //frame.setSize(400, 300);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(screenSize.width, screenSize.height); // Set frame size to the screen's resolution
+        frame.setLayout(new MigLayout("fill", "[grow][right]", "[]20[grow]"));
 
         NavBar navbar = new NavBar(0);
-        frame.add(navbar.displayNavBar(), "cell 1 0, growx");
-        frame.add(getHomePage(), "cell 0 1, span, growx"); // Makes elements automatically span across all columns
+        frame.add(navbar.displayNavBar(), "cell 1 0, align right, growx");
+        frame.add(getHomePage(), "cell 0 1, span, center"); // Makes elements automatically span across all columns
 
-        frame.setLocationRelativeTo(null);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
     }
 
