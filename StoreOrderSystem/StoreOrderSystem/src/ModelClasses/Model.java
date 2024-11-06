1f496eb81ce1e8a2bed58e2818011be81bc03d34
LoneWeasel <robertacooke04@gmail.com>
2024-11-06 16:39:15 -0500

    Jackson Setup
    File Error but no Jackson Errors

--- StoreOrderSystem/StoreOrderSystem/src/ModelClasses/Model.java
+++ StoreOrderSystem/StoreOrderSystem/src/ModelClasses/Model.java
@@ -1,9 +1,66 @@
 package ModelClasses;
-import org.json.JSONObject;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class Model {
-    public static void main(String [] args){
-        JSONObject jo = new JSONObject("{ \"abc\" : \"def\" }");
-        System.out.println(jo);
+
+    public static void main(String[] args) {
+        // Create an ObjectMapper instance
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            // Read the JSON array and map it to a List of Person objects
+            List<Person> people = objectMapper.readValue(new File("src/ModelClasses/Database.json"), new TypeReference<List<Person>>(){});
+
+            // Print out the people
+            for (Person person : people) {
+                System.out.println("Name: " + person.getName());
+                System.out.println("Age: " + person.getAge());
+                System.out.println("Email: " + person.getEmail());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+class Person {
+    private String name;
+    private int age;
+    private String email;
+
+    // Default constructor (required for Jackson)
+    public Person() {}
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+
